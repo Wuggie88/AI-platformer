ddb048d76d4023ea9828406c2b4e4e28672e769d
Wuggie <wuggie88@yahoo.co.uk>
2022-05-06 11:02:28 +0200

    Player movement

---
+++ Unishoe/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 0.7f;
+    bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //firing script
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
